c9c8e2d5b0ae4505ea57614a978169e715caa1eb
Kyle Coffey <mariorules24@gmail.com>
2021-12-03 12:11:10 -0600

    day 01

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    let input = std::fs::read_to_string("input.txt").unwrap();
+    let mut counter = 0;
+    for (i, c) in input.char_indices() {
+        match c {
+            '(' => counter += 1,
+            ')' => {
+                counter -= 1;
+                if counter < 0 {
+                    println!("{}", i + 1);
+                    break;
+                }
+            }
+            _ => (),
+        }
+    }
 }
