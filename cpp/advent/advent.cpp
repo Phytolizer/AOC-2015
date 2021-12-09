27f87557842d0981af9d52afe43e0b0b3a86186d
Kyle Coffey <mariorules24@gmail.com>
2021-12-09 16:12:10 -0600

    Day 2

--- cpp/advent/advent.cpp
+++ cpp/advent/advent.cpp
@@ -4,7 +4,11 @@
 
 std::string advent::readEntireFile(const std::string& fileName)
 {
-    std::ifstream file{fileName};
+    const std::ifstream file{fileName};
+    if (!file.is_open())
+    {
+        throw std::runtime_error{"Failed getting puzzle input: Could not open file"};
+    }
     std::ostringstream contents;
     contents << file.rdbuf();
     return contents.str();
--- cpp/advent/inc/advent.hpp
+++ cpp/advent/inc/advent.hpp
@@ -3,6 +3,7 @@
 #include <string>
 
 #define ADVENT_FOREACH_CHAR(File, CharVar) while ((File) >> (CharVar))
+#define ADVENT_FOREACH_LINE(File, LineVar) while (std::getline((File), (LineVar)))
 
 namespace advent
 {
