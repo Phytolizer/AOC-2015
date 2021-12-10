b2b28351962f693094580da35c9743e0034dcde2
Kyle Coffey <mariorules24@gmail.com>
2021-12-10 08:33:41 -0600

    day 3

--- cpp/advent/inc/advent.hpp
+++ cpp/advent/inc/advent.hpp
@@ -8,4 +8,10 @@
 namespace advent
 {
 std::string readEntireFile(const std::string& fileName);
+
+template <typename T, typename... Rest> void hashCombine(size_t& seed, const T& v, Rest... rest)
+{
+    seed ^= std::hash<T>{}(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+    (hashCombine(seed, rest), ...);
 }
+} // namespace advent
