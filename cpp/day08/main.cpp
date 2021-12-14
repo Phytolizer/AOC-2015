ab6368024d83fd873edb5fe5d6857e01ac281949
Kyle Coffey <mariorules24@gmail.com>
2021-12-13 19:20:14 -0600

    start work on C++ day 8

--- cpp/day08/main.cpp
+++ cpp/day08/main.cpp
@@ -1,5 +1,22 @@
 #include <advent.hpp>
+#include <ctre.hpp>
+#include <fstream>
+
+constexpr ctll::fixed_string HEX_RE = R"(\\x[0-9a-f]{2})";
+constexpr ctll::fixed_string QUOTE_RE = R"(\\\")";
+constexpr ctll::fixed_string SLASH_RE = R"(\\\\)";
 
 int main()
 {
+    std::ifstream input{"day08/input.txt"};
+    std::string line;
+    ADVENT_FOREACH_LINE(input, line)
+    {
+        if (line.empty())
+        {
+            continue;
+        }
+
+        std::string origLine = line;
+    }
 }
