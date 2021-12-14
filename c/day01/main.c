4f8d9619bb518fd5705a1508e104488b1a66c249
Kyle Coffey <mariorules24@gmail.com>
2021-12-13 23:42:29 -0600

    Day 01 in C

--- c/day01/main.c
+++ c/day01/main.c
@@ -1,5 +1,7 @@
 #include <advent.h>
+#include <stdbool.h>
 #include <stddef.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 int main(void)
@@ -9,6 +11,32 @@ int main(void)
     {
         return 1;
     }
+
+    int floor = 0;
+    size_t index = 1;
+    bool indexFound = false;
+
+    for (char* c = input; *c != '\0'; c++)
+    {
+        switch (*c)
+        {
+        case '(':
+            ++floor;
+            break;
+        case ')':
+            --floor;
+            if (!indexFound && floor < 0)
+            {
+                indexFound = true;
+                printf("Part 2: %zu\n", index);
+            }
+            break;
+        default:
+            break;
+        }
+        ++index;
+    }
+    printf("Part 1: %d\n", floor);
 
     free(input);
     return 0;
