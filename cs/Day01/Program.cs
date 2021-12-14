152975b3868d8856c0d8ad0baf9431af45b6af76
Kyle Coffey <mariorules24@gmail.com>
2021-12-13 21:12:22 -0600

    Day 1 in C#

--- cs/Day01/Program.cs
+++ cs/Day01/Program.cs
@@ -1,3 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
+var input = File.ReadAllText("Input.txt");
+var floor = 0;
+var index = 1;
+var indexFound = false;
+foreach (var c in input)
+{
+    switch (c)
+    {
+        case '(':
+            ++floor;
+            break;
+        case ')':
+            --floor;
+            if (!indexFound && floor < 0)
+            {
+                Console.WriteLine($"Part 2: {index}");
+                indexFound = true;
+            }
+
+            break;
+    }
+
+    ++index;
+}
+
+Console.WriteLine($"Part 1: {floor}");
