94353a351778320beede1da6a1a9f6efa2127649
Kyle Coffey <mariorules24@gmail.com>
2021-12-14 19:40:53 -0600

    C# Day 02

--- cs/Day02/Program.cs
+++ cs/Day02/Program.cs
@@ -1,3 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text.RegularExpressions;
+using Combinatorics.Collections;
+using Helper;
 
-Console.WriteLine("Hello, World!");
+var lines = File.ReadAllLines("Input.txt");
+var linePattern = new Regex(@"(\d+)x(\d+)x(\d+)");
+var result = 0;
+var result2 = 0;
+foreach (var line in lines)
+{
+    var match = linePattern.Match(line);
+    var l = int.Parse(match.Groups[1].Value);
+    var w = int.Parse(match.Groups[2].Value);
+    var h = int.Parse(match.Groups[3].Value);
+    var sides = new[]
+    {
+        l * w,
+        w * h,
+        h * l
+    };
+    result += 2 * sides.Sum() + sides.Min();
+    result2 += new Combinations<int>(new[] { l, w, h }, 2, GenerateOption.WithoutRepetition)
+        .Select(pair => 2 * pair[0] + 2 * pair[1]).Min() + l * w * h;
+}
+
+Console.WriteLine($"Part 1: {result}");
+Console.WriteLine($"Part 2: {result2}");
---
+++ cs/Helper/EnumerableUtils.cs
@@ -0,0 +1,17 @@
+﻿namespace Helper;
+
+public static class EnumerableUtils
+{
+    public static IEnumerable<(T, T)> GetUniquePairs<T>(IEnumerable<T> items)
+    {
+        var itemsArray = items as T[] ?? items.ToArray();
+        for (var index = 0; index < itemsArray.Length; index++)
+        {
+            var item = itemsArray[index];
+            foreach (var result in itemsArray.Skip(index + 1))
+            {
+                yield return (item, result);
+            }
+        }
+    }
+}
