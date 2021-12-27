cbd606a5db21566c3ac862cb39fa7cfed5e0e6aa
Kyle Coffey <mariorules24@gmail.com>
2021-12-27 02:16:10 -0600

    complete day 15

--- c/day15/main.c
+++ c/day15/main.c
@@ -21,6 +21,9 @@ typedef struct {
 DECLARE_VECTOR(ingredient_vec_t, ingredient_t);
 DEFINE_VECTOR(ingredient_vec_t, ingredient_t);
 
+static size_t score_combo(ingredient_vec_t* ingredients,
+                          advent_combinatorics_sum_comb_t* comb);
+
 int main(void) {
   FILE* fp = fopen(ADVENT_INPUT, "r");
   if (fp == NULL) {
@@ -72,14 +75,52 @@ int main(void) {
     ingredient_vec_t_push(&ingredients, ingredient);
   }
 
-  advent_combinatorics_sum_comb_t comb =
-      advent_combinatorics_first_sum_comb(4, 8);
-  do {
-    for (size_t i = 0; i < 4; ++i) {
-      printf("%d ", comb.data[i]);
-    }
-    printf("\n");
-  } while (advent_combinatorics_next_sum_comb(&comb, 8));
+  size_t best_score = 0;
 
+  advent_combinatorics_sum_comb_t comb =
+      advent_combinatorics_first_sum_comb(ingredients.length, 100);
+  do {
+    size_t combo = score_combo(&ingredients, &comb);
+    if (combo > best_score) {
+      best_score = combo;
+    }
+  } while (advent_combinatorics_next_sum_comb(&comb, 100));
+  advent_combinatorics_sum_comb_t_deinit(&comb);
+
+  printf("%zu\n", best_score);
+
+  free(line);
+  fclose(fp);
+  pcre2_match_data_free(match_data);
+  pcre2_code_free(code);
+  for (size_t i = 0; i < ingredients.length; ++i) {
+    string_free(ingredients.data[i].name);
+  }
+  ingredient_vec_t_deinit(&ingredients);
   return 0;
+}
+
+static size_t score_combo(ingredient_vec_t* ingredients,
+                          advent_combinatorics_sum_comb_t* comb) {
+  ingredient_t resulting_combo = {0};
+  for (size_t i = 0; i < ingredients->length; i++) {
+    ingredient_t* ingredient = &ingredients->data[i];
+    int amount = comb->data[i];
+    resulting_combo.capacity += ingredient->capacity * amount;
+    resulting_combo.durability += ingredient->durability * amount;
+    resulting_combo.flavor += ingredient->flavor * amount;
+    resulting_combo.texture += ingredient->texture * amount;
+    resulting_combo.calories += ingredient->calories * amount;
+  }
+
+  if (resulting_combo.capacity < 0 || resulting_combo.durability < 0 ||
+      resulting_combo.flavor < 0 || resulting_combo.texture < 0) {
+    return 0;
+  }
+  if (resulting_combo.calories != 500) {
+    return 0;
+  }
+
+  return resulting_combo.capacity * resulting_combo.durability *
+         resulting_combo.flavor * resulting_combo.texture;
 }
